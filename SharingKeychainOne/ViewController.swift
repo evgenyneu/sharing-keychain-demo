6712222c12f70f5d98c1dd279d591f049a6967c4
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-12-19 11:39:37 +1100

    Initial commit

---
+++ SharingKeychainOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharingKeychainOne
+//
+//  Created by Evgenii on 19/12/2015.
+//  Copyright © 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
